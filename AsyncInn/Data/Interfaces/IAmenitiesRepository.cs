c9833e6bae8141feef912f2b9b1f66a19d0e83b5
Marie Antons <chefantons@gmail.com>
2020-05-26 16:27:44 -0500

    UpdateAmenities complete

--- AsyncInn/Data/Interfaces/IAmenitiesRepository.cs
+++ AsyncInn/Data/Interfaces/IAmenitiesRepository.cs
@@ -15,7 +15,7 @@ namespace AsyncInn.Data.Interfaces
         Task<Amenities> GetAmenitiesById(int id);
 
         //U: Update
-        
+        Task<Amenities> UpdateAmenities(Amenities amenities);
 
         //D: Delete
         

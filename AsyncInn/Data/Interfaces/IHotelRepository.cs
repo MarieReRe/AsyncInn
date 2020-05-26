e4d4bf0be74706416265675b0bc51d4ecda1e94d
Marie Antons <chefantons@gmail.com>
2020-05-26 12:06:27 -0500

    hotel interface creation

--- AsyncInn/Data/Interfaces/IHotelRepository.cs
+++ AsyncInn/Data/Interfaces/IHotelRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AsyncInn.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,18 @@ namespace AsyncInn.Data.Interfaces
 {
     interface IHotelRepository
     {
+
+        //C: Create
+        Task CreateHotel(Hotel hotel);
+
+        //R: Read
+        Task<Hotel> GetHotelById(long id);
+        Task<List<Hotel>> GetHotels();
+
+        //U: Update
+        Task UpdateHotel(Hotel hotel);
+
+        //D: Delete
+        Task DeleteHotel(int id);
     }
 }

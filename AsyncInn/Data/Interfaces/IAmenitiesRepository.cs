5cfa0257964be47f617e108d9247f9aa017753b4
Marie Antons <chefantons@gmail.com>
2020-05-26 16:21:32 -0500

    Creation of IAmenities and creation of amenities complete

---
+++ AsyncInn/Data/Interfaces/IAmenitiesRepository.cs
@@ -0,0 +1,23 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data.Interfaces
+{
+   public interface IAmenitiesRepository
+    {
+        //C: Create
+        Task CreateAmenities(Amenities amenities);
+
+        //R: Read
+       
+
+        //U: Update
+        
+
+        //D: Delete
+        
+    }
+}

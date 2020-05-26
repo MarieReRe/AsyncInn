f63674dcc3dcc283e9f18c55087f79c3a6d8c7b3
Marie Antons <chefantons@gmail.com>
2020-05-26 16:08:08 -0500

    Start of room interface, room created

---
+++ AsyncInn/Data/Interfaces/IRoomRepository.cs
@@ -0,0 +1,24 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data.Interfaces
+{
+   public interface IRoomRepository
+    {
+        //C: Create
+        Task CreateRoom(Room room);
+
+        //R: Read
+      
+   
+
+        //U: Update
+    
+
+        //D: Delete
+    
+    }
+}

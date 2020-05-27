ca1b36baae9e2c741f03892f710710add2cf2a08
Marie Antons <chefantons@gmail.com>
2020-05-27 11:00:10 -0500

    implemented GetAmenitiesById

--- AsyncInn/Controllers/AmenitiesController.cs
+++ AsyncInn/Controllers/AmenitiesController.cs
@@ -31,9 +31,9 @@ namespace AsyncInn.Controllers
 
         // GET: api/Amenities/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Amenities>> GetAmenities(int id)
+        public async Task<ActionResult<Amenities>> GetAmenities(int amenitiesId)
         {
-            var amenities = await _context.Amenities.FindAsync(id);
+            Amenities amenities = await amenitiesRepository.GetAmenitiesById(amenitiesId);
 
             if (amenities == null)
             {
@@ -44,8 +44,6 @@ namespace AsyncInn.Controllers
         }
 
         // PUT: api/Amenities/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for
-        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
         public async Task<IActionResult> PutAmenities(int id, Amenities amenities)
         {

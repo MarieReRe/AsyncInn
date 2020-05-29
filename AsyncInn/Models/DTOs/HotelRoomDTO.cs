fcd2cf49f80e1654555b7abeec0a9f673c8be404
Keith Dahlby <keith@newbo.co>
2020-05-29 10:12:28 -0500

    Fixes for GET /api/Hotels

--- AsyncInn/Models/DTOs/HotelRoomDTO.cs
+++ AsyncInn/Models/DTOs/HotelRoomDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AsyncInn.Models.DTOs
+﻿namespace AsyncInn.Models.DTOs
 {
     public class HotelRoomDTO
     {
@@ -11,7 +6,6 @@ namespace AsyncInn.Models.DTOs
         public int RoomNumber { get; set; }
         public string Name { get; set; }
         public decimal Rate { get; set; }
-        public int RoomID { get; set; }
         public RoomDTO Room { get; set; }
     }
 }

af39c9e1ae7b7ca3e69ed4bd31afe31629415849
Marie Antons <chefantons@gmail.com>
2021-02-21 12:52:34 -0600

    Add Hotel Department

---
+++ AsyncInn/Models/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class Department
+    {
+        public long Id { get;set;}
+
+        public long Hotel_Id { get; set; }
+
+        public Hotel Hotel { get; set; }
+
+        public string Name { get; set; }
+
+        public string PhoneNumber { get; set; }
+
+        public string ExtensionNumber { get; set; }
+
+        public string Responsibilities { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+    }
+}
